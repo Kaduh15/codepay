7647c8e39de66dd010668be6a25b0f6fbde321b7
kaduh15 <kadu.silva2014@gmail.com>
2024-02-24 20:15:01 -0300

    feat: adiciona cor em cada etapa de formulário
    A cada avanço de etapa, a cor do ícone e do texto da etapa é alterada para verde.

--- src/components/aside/index.tsx
+++ src/components/aside/index.tsx
@@ -1,16 +1,25 @@
+import { ShoppingCartStep } from '@/types'
 import { Package, User, Wallet } from 'lucide-react'
 
-export default function Aside() {
+type AsideProps = {
+  step: ShoppingCartStep
+}
+
+function activeStep(step: number, active: number) {
+  return step >= active ? 'text-green-500' : ''
+}
+
+export default function Aside({ step = 1 }: AsideProps) {
   return (
     <aside className="flex w-full justify-start gap-6">
-      <User />
-      Conta
+      <User className={activeStep(step, 1)} />
+      <span className={activeStep(step, 1)}>Conta</span>
       <div>-</div>
-      <Package />
-      Envio
+      <Package className={activeStep(step, 2)} />
+      <span className={activeStep(step, 2)}>Envio</span>
       <div>-</div>
-      <Wallet />
-      Pagamento
+      <Wallet className={activeStep(step, 3)} />
+      <span className={activeStep(step, 3)}>Pagamento</span>
     </aside>
   )
 }
---
+++ src/types.ts
@@ -0,0 +1,5 @@
+export enum ShoppingCartStep {
+  ACCOUNT = 1,
+  PAYMENT = 2,
+  SHIPPING = 3,
+}

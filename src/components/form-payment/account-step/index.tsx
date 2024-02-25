58a447e098113843f012db3cc0015ddb198c8c94
kaduh15 <kadu.silva2014@gmail.com>
2024-02-25 11:23:32 -0300

    feat: usa o novo componente de input

--- src/components/form-payment/account-step/index.tsx
+++ src/components/form-payment/account-step/index.tsx
@@ -1,26 +1,42 @@
 import Link from 'next/link'
 
+import Button from '@/components/ui/button'
+import Input from '@/components/ui/input'
+
 export default function AccountStep() {
   return (
-    <>
-      <h3>Detalhes da conta</h3>
+    <section className="space-y-12">
+      <h2 className="justify-self-start-start text-xl font-semibold text-slate-800">
+        Detalhes da conta
+      </h2>
 
-      <div>
-        <label htmlFor="email">Email</label>
-        <input type="email" id="email" />
-      </div>
+      <Input
+        label="Email"
+        id="email"
+        name="email"
+        placeholder="email@email.com"
+      />
 
-      <div>
-        <label htmlFor="password">Senha</label>
-        <input type="password" id="password" />
-      </div>
-
-      <div>
-        <Link rel="stylesheet" href="#">
-          Registrar conta
+      <Input
+        label="Senha"
+        type="password"
+        id="password"
+        name="password"
+        placeholder="********"
+      />
+      <div className="space-x-12 text-end">
+        <Link className="font-medium" rel="stylesheet" href="#">
+          Registre-se para conta
         </Link>
-        <button>Entrar</button>
+        <Button
+          variants={{
+            style: 'primary',
+            size: 'lg',
+          }}
+        >
+          Entrar
+        </Button>
       </div>
-    </>
+    </section>
   )
 }
